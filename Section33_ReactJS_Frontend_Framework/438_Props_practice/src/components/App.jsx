21beff7d3eeef8cbdc5f98897a905843286cc672
Lee YoungCheol <ofetwo@gmail.com>
2020-10-27 23:09:14 +0900

    Update section33

--- Section33_ReactJS_Frontend_Framework/438_Props_practice/src/components/App.jsx
+++ Section33_ReactJS_Frontend_Framework/438_Props_practice/src/components/App.jsx
@@ -7,16 +7,16 @@ import Info from "./Info";
 
 function createCard(contact) {
   return (
-    <Card 
-      // key는 JSX에서 map()이 사용될때 자동으로 생성되는 특별한 속성이다. 
-      // 사용한다고해서 에러가 발생되지는 않지만 재사용될 수 없다.
+    <Card
+      {/* key는 JSX에서 map()이 사용될때 자동으로 생성되는 특별한 속성이다. */}
+      {/* 사용한다고해서 에러가 발생되지는 않지만 브라우저에 나타나지는 않는다. */}
       key={contact.id}
-      // 따라서 id 값을 이용하기 위해 다른 속성 이름이 필요하다.
+      {/* 따라서 id 값을 이용하기 위해서는 다른 속성 이름이 필요하다. */}
       id={contact.id}
-      name={contact.name} 
-      img={contact.imgURL} 
-      tel={contact.phone} 
-      email={contact.email} 
+      name={contact.name}
+      img={contact.imgURL}
+      tel={contact.phone}
+      email={contact.email}
     />
   );
 }
@@ -30,32 +30,35 @@ function App() {
       <Info detailInfo="Hello I'm Chalie."/>
       {/* Card components들의 구조를 체계적으로 구성 */}
       <h1 className="heading">My Contacts</h1>
-      {/* <Card 
-        name={contacts[0].name} 
-        img={contacts[0].imgURL} 
-        tel={contacts[0].phone} 
-        email={contacts[0].email} 
+      {/* <Card
+        name={contacts[0].name}
+        img={contacts[0].imgURL}
+        tel={contacts[0].phone}
+        email={contacts[0].email}
       />
-      <Card 
-        name={contacts[1].name} 
-        img={contacts[1].imgURL} 
-        tel={contacts[1].phone} 
-        email={contacts[1].email} 
+      <Card
+        name={contacts[1].name}
+        img={contacts[1].imgURL}
+        tel={contacts[1].phone}
+        email={contacts[1].email}
       />
-      <Card 
-        name={contacts[2].name} 
-        img={contacts[2].imgURL} 
-        tel={contacts[2].phone} 
-        email={contacts[2].email} 
+      <Card
+        name={contacts[2].name}
+        img={contacts[2].imgURL}
+        tel={contacts[2].phone}
+        email={contacts[2].email}
       /> */}
 
       {/* JSX내에서 표현문은 가능하지만 구문은 사용이 불가능하기 때문에 if 나 for 또는 while 등은 사용할 수 없다. */}
-      {/* 그런데 자바스크립트 메소드 중에는 for 구문과 유사한 forEach()가 있고, 또 이를 포함하고 있는 map(), filter(), reduce() 메소드가 있다. */}
-      {/* { contacts.map(function(contact) {return createCard(contact);}) } */}
+      {/* 그런데 자바스크립트 메소드 중에는 for 구문과 유사한 forEach()가 있고, 또 이를 포함하고 있는 map()을 아래와 이용할 수 있다. */}
+      {/* { contacts.map(function(contact) {
+        return createCard(contact);
+        }) }
+      또는
+      */}
+      { contacts.map(createCard) }
       {/* 특이점 첫번째, 표현문만 가능하기 때문에  map(); 으로 작성하지 않는다. 세미콜론은 제거 해야한다. */}
       {/* 특이점 두번째, 첫번째 특징에 의한 react complile의 차이인지 모르겠지만 map()의 실행결과가 배열로 반환되지 않는다. */}
-      {/* 콜백 함수와의 관계를 설명하기 위해 위와 같이 설명하였고 어쨌든 결론적으로 아래와 같이 심플하게 표현할 수 있다. */}
-      { contacts.map(createCard) }
     </div>
   );
 }
--- Section33_ReactJS_Frontend_Framework/438_Props_practice/src/index.js
+++ Section33_ReactJS_Frontend_Framework/438_Props_practice/src/index.js
@@ -8,16 +8,16 @@ import App from "./components/App";
 // => styles.css 파일을 보고 App.jsx에 적합한 'className'들을 추가
 
 //2. Extract the contact card as a reusable Card component.
-// => App.jsx에서 반복되는 구조의 재사용 가능한 요소들만 뽑아내어 Card.jsx 생성 
+// => App.jsx에서 반복되는 구조의 재사용 가능한 요소들만 뽑아내어 Card.jsx 생성
 
 //3. Use props to render the default Beyonce contact card
 //so the Card component can be reused for other contacts.
-// => App.jsx의 <Card />에 사용되는 속성들을 Card.jsx에서 props를 이용해 추가 
+// => App.jsx의 <Card />에 사용되는 속성들을 Card.jsx에서 props를 이용해 추가
 
 //4. Import the contacts.js file to create card components.
-//=> 아래와 같이 해결
+//=> contacts.js 파일에서 export하고 App.jsx에서 import
 
-ReactDOM.render(  
+ReactDOM.render(
     <App />,
     document.getElementById("root")
 );
